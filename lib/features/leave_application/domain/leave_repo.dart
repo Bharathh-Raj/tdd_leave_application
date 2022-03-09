cc6697f45e8f06b9e275415d0eee20119b77b653
Bharath <bt9944@gmail.com>
2022-03-09 14:36:55 +0530

    Create leave_repo.dart

---
+++ lib/features/leave_application/domain/leave_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:leave_application/features/leave_application/domain/leave_application.dart';
+
+import '../../../core/failure.dart';
+
+abstract class LeaveRepo {
+  Future<Either<Failure, void>> applyLeave(LeaveApplication leaveApplication);
+  Future<Either<Failure, void>> updateLeave(LeaveApplication leaveApplication);
+  Future<Either<Failure, void>> deleteLeave(LeaveApplication leaveApplication);
+  Future<Either<Failure, List<LeaveApplication>>> fetchLeaveApplications();
+}
